6e8f6b87a0f3022ef497297fffad5be5df942e50
snowlight-aemt <it.s.aemt@gmail.com>
2023-03-02 21:29:06 +0900

    INit

---
+++ src/test/java/me/snowlight/productmanagement/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snowlight.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
